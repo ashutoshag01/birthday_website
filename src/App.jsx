e50c65cb19a861d1c8d36a1ee925b5e8315103f6
ashutoshag01 <33784149+ashutoshag01@users.noreply.github.com>
2025-07-31 19:12:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ export default function InteractiveUniverse() {
         <div className="absolute inset-0 bg-black bg-opacity-80 flex flex-col items-center justify-center text-white z-30">
           <div className="w-32 h-32 rounded-full bg-white mb-6 overflow-hidden">
             <img
-              src="https://photos.app.goo.gl/f8Q2fmLTu7jYD7j26"
+              src="images/Profile.jpg"
               alt="Profile"
               className="w-full h-full object-cover"
             />
